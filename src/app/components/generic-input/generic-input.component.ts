a5009e9f53ed888a4d575871ad5f8a1f7685ee8e
harald.hiemstra <haraldhiemstra@Haralds-MBP.corp.capgemini.com>
2023-09-11 15:52:47 +0200

    usage of tables in formfields

---
+++ src/app/components/generic-input/generic-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormCell } from 'src/app/services/formspec/form-spec.interface';
+
+@Component({
+  selector: 'app-generic-input',
+  templateUrl: './generic-input.component.html',
+  styleUrls: ['./generic-input.component.scss'],
+})
+export class GenericInputComponent {
+  @Input() form!: FormGroup;
+
+  @Input() formCell!: FormCell;
+
+  get id() {
+    return this.formCell.aspects.concept.localPart;
+  }
+}
